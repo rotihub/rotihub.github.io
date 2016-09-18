b64e3614770d2ff395ce7b98a60fb228d0bd0259
rotihub <rohitt.tiwari@gmail.com>
2016-09-18 12:39:50 +0100

    Update text_to_speech_extension.js

--- scratch-extensions/text-to-speech/text_to_speech_extension.js
+++ scratch-extensions/text-to-speech/text_to_speech_extension.js
@@ -32,7 +32,7 @@ new (function() {
     //    var u = new SpeechSynthesisUtterance(text.toString());
         var u = new SpeechSynthesisUtterance("Rohit");    
         u.pitch = 0.01;
-        u.lang = 'en-GB';
+        u.lang = 'es';
         u.onend = function(event) {
             if (typeof callback=="function") callback();
         };
