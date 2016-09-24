7de3dd172147254780f343ba2f5697a42f38186b
ro <rohitt.tiwari@gmail.com>
2016-09-24 16:46:44 +0100

    Signed-off-by: ro

--- pico/picoext1.js
+++ pico/picoext1.js
@@ -104,6 +104,7 @@
 
         device = dev;
         device.open({ stopBits: 0, bitRate: 115200, ctsFlowControl: 0 });
+        console.log('Port opened');
         device.set_receive_handler(function(data) {
             console.log('Received: ' + data.byteLength);
             if(!rawData || rawData.byteLength == 18) rawData = new Uint8Array(data);
